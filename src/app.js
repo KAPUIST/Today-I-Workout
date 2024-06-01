1e766ce8a94261fcfb8e66ce8e17ebb59b9831c9
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-06-01 12:11:42 +0900

    Update - 서버 실행 로직

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { connectDatabase } from "./utils/prisma.util.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
-await connectDatabase();
-app.listen(3000, () => {
-    console.log("?");
-});
+const startServer = async () => {
+    try {
+        await connectDatabase();
+        app.listen(3000, () => {
+            console.log("서버가 3000번 포트에서 실행 중입니다.");
+        });
+    } catch (error) {
+        console.error("데이터베이스 연결 실패:", error);
+        process.exit(1); // 실패 코드로 프로세스 종료
+    }
+};
+
+startServer();
