0522e4cc17e51b99a967d636e385108c0199f492
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-06-01 12:21:11 +0900

    fix - prisma.util 파일 트라이 캐치 구문 제거

--- src/utils/prisma.util.js
+++ src/utils/prisma.util.js
@@ -5,9 +5,5 @@ export const prisma = new PrismaClient({
     errorFormat: "pretty"
 });
 export async function connectDatabase() {
-    try {
-        await prisma.$connect();
-    } catch (error) {
-        console.error("데이터베이스 연결에 실패했습니다:", error);
-    }
+    await prisma.$connect();
 }
