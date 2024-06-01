dd6a6b1daeb89164f53542e57e539f9b39d5183d
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-06-01 11:53:17 +0900

    Feat - AWS RDS 연결 구현

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { connectDatabase } from "./utils/prisma.util.js";
 
 const app = express();
 
+await connectDatabase();
 app.listen(3000, () => {
     console.log("?");
 });
