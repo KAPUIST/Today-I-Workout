21811087d775e223f30db72a05361706d3d08ccd
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-06-01 12:24:35 +0900

    Fix - 서버연결 콘솔로그 수정

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const startServer = async () => {
     try {
         await connectDatabase();
         app.listen(PORT, () => {
-            console.log("서버가 3000번 포트에서 실행 중입니다.");
+            console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
         });
     } catch (error) {
         console.error("데이터베이스 연결 실패:", error);
