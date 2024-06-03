452c8e32ce7932ac6f24c350a02af3056260e889
Budubuu <162087289+Budubuu@users.noreply.github.com>
2024-06-03 11:46:34 +0900

    dev 병합 완료

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { connectDatabase } from "./utils/prisma/prisma.util.js";
-import { connectDatabase } from "./utils/prisma/prisma.util.js";
 import dotenv from "dotenv";
 import authRouter from "./routers/auth.router.js";
 import userRouter from "./routers/user.router.js";
 import postRouter from "./routers/post.router.js";
 import commentRouter from "./routers/comment.router.js";
 import errorHandler from "./utils/validator/customErrorHandler.js";
-import CustomErrorHandler from "./middlewares/error.middleware.js";
+import { CustomErrorHandler } from "./middlewares/error.middleware.js";
 import { SERVER_PORT } from "./constants/env.constant.js";
 import cookieParser from "cookie-parser";
 import STATUS_CODES from "./constants/status.constant.js";
@@ -32,8 +31,6 @@ app.use(CustomErrorHandler);
 const startServer = async () => {
     try {
         await connectDatabase();
-        app.listen(SERVER_PORT, () => {
-            console.log(`서버가 ${SERVER_PORT}번 포트에서 실행 중입니다.`);
         app.listen(SERVER_PORT, () => {
             console.log(`서버가 ${SERVER_PORT}번 포트에서 실행 중입니다.`);
         });
