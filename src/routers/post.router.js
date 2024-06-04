22c9abbd592d2e2404d4615a90188cb1d1f59b89
hyein <mickey_8688@naver.com>
2024-06-04 14:08:03 +0900

    [Feat] - 댓글 생성, 조회 코드분리 01

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import STATUS_CODES from "../constants/status.constant.js";
-import { fetchPostsByPostType } from "../services/post.service.js";
+import { fetchPostsByPostType, createNewComment } from "../services/post.service.js";
 import ErrorHandler from "../utils/customErrorHandler.js";
 import { requireAccessToken } from "../middlewares/auth.middleware.js";
 import { prisma } from "../utils/prisma/prisma.util.js";
@@ -52,25 +52,26 @@ router.post("/posts/:postId/comments", requireAccessToken, async (req, res, next
 
         // 1-1 게시글이 존재하는지 확인하기
         const post = await prisma.post.findFirst({
-            where:  {
-                id: postId,
-            },
+            where: {
+                id: postId
+            }
         });
 
         // 1-2 게시글이 존재하지 않을때 메시지 반환
         if (!post) {
             throw new ErrorHandler(STATUS_CODES.BAD_REQUEST, "게시글이 존재하지 않습니다");
         }
-        // 댓글생성 1-3 댓글을 데이터베이스에 생성
-        const newComment = await prisma.comment.create({
-            data: {
-                user_id: id,
-                post_id: postId,
-                content: content,
-            },
-        });
+        // // 댓글생성 1-3 댓글을 데이터베이스에 생성
+        // const newComment = await prisma.comment.create({
+        //     data: {
+        //         user_id: id,
+        //         post_id: postId,
+        //         content: content,
+        //     },
+        // });
+        const comment = await createNewComment(id, postId, content);
         // 1-4 성공했을때 댓글 데이터 반환
-        res.status(STATUS_CODES.CREATED).json({ data: newComment });
+        res.status(STATUS_CODES.CREATED).json({ data: comment });
     } catch (error) {
         next(error);
     }
@@ -83,8 +84,8 @@ router.get("/posts/:postId/comments", async (req, res, next) => {
         // 2-1 게시글이 존재하는지 확인
         const post = await prisma.post.findFirst({
             where: {
-                id: postId,
-            },
+                id: postId
+            }
         });
         // 2-2 게시글이 존재하지 않을때 메시지 반환
         if (!post) {
@@ -93,11 +94,11 @@ router.get("/posts/:postId/comments", async (req, res, next) => {
         // 2-3 해당 게시글에 대한 댓글 불러오기
         const comments = await prisma.comment.findMany({
             where: {
-                post_id: postId,
+                post_id: postId
             },
             orderBy: {
-                created_at: 'desc', // 댓글 생성일시 내림차순 정렬
-            },
+                created_at: "desc" // 댓글 생성일시 내림차순 정렬
+            }
         });
         // 2-4 성공적으로 불러온 댓글 데이터 반환
         res.status(STATUS_CODES.OK).json({ data: comments });
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -19,3 +19,15 @@ export const fetchPostsByPostType = async (postType, orderBy) => {
     }
     return await prisma.post.findMany({ where: { post_type: postType }, orderBy: order });
 };
+
+// 댓글생성 1-3 댓글을 데이터베이스에 생성
+export const createNewComment = async (id, postId, content) => {
+    const newComment = await prisma.comment.create({
+        data: {
+            user_id: id,
+            post_id: postId,
+            content: content
+        }
+    });
+    return newComment;
+};
